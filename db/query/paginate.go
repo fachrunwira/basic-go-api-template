ac2b4a7a1a80a8b69349b6a9cd8e9041f8bc276b
fachrunwira <fachrunwirap@gmail.com>
2025-07-14 23:14:34 +0700

    updt: finished pagination links

--- db/query/paginate.go
+++ db/query/paginate.go
@@ -10,6 +10,12 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type navLinks struct {
+	Active bool
+	Label  string
+	Url    string
+}
+
 func (qb *queryBuilder) countRows() (*int, error) {
 	query := fmt.Sprintf("SELECT count(*) FROM %s", qb.tableName)
 
@@ -36,14 +42,77 @@ func (qb *queryBuilder) countRows() (*int, error) {
 	return &total, nil
 }
 
-func setUri(c echo.Context, links string) string {
+func navigationLinks(c echo.Context, page, last_page int) []interface{} {
+	var links []navLinks
+	showBefore, showAfter := 5, 5
+	start := max(1, page-showBefore)
+	end := min(last_page, page+showAfter)
+
+	if last_page <= (showAfter + showBefore + 3) {
+		start = 1
+		end = last_page
+	}
+
+	if page > 1 {
+		links = append(links, navLinks{Active: false, Label: "&laquo Prev", Url: setNavigation(c, page-1)})
+	} else {
+		links = append(links, navLinks{Active: false, Label: "&laquo Prev", Url: ""})
+	}
+
+	if start > 1 {
+		links = append(links, navLinks{Active: false, Label: "1", Url: setNavigation(c, 1)})
+		if start > 2 {
+			links = append(links, navLinks{Active: false, Label: "...", Url: ""})
+		}
+	}
+
+	for i := start; i <= end; i++ {
+		links = append(links, navLinks{Active: i == page, Label: strconv.Itoa(i), Url: setNavigation(c, i)})
+	}
+
+	if end < last_page {
+		if end < last_page-1 {
+			links = append(links, navLinks{Active: false, Label: "...", Url: ""})
+		}
+		links = append(links, navLinks{Active: false, Label: strconv.Itoa(last_page), Url: setNavigation(c, last_page)})
+	}
+
+	if last_page > page {
+		links = append(links, navLinks{Active: false, Label: "&raquo", Url: setNavigation(c, page+1)})
+	} else {
+		links = append(links, navLinks{Active: false, Label: "&raquo", Url: ""})
+	}
+
+	newLinks := []interface{}{}
+	for _, v := range links {
+		list := map[string]interface{}{
+			"active": v.Active,
+			"label":  v.Label,
+			"url":    v.Url,
+		}
+		newLinks = append(newLinks, list)
+	}
+
+	return newLinks
+}
+
+func setNavigation(c echo.Context, page int) string {
 	host := env.Get("APP_URL", fmt.Sprintf("%s://%s", c.Scheme(), c.Request().Host))
 	path := c.Request().URL.Path
+
+	queryParams := setUri(c)
+	queryParams["page"] = strconv.Itoa(page)
+
+	var newQueryParams []string
+	for k, v := range queryParams {
+		newQueryParams = append(newQueryParams, fmt.Sprintf("%s=%v", k, v))
+	}
+	return fmt.Sprintf("%s%s?%s", host, path, strings.Join(newQueryParams, "&"))
+}
+
+func setUri(c echo.Context) map[string]string {
 	uri := c.Request().RequestURI
-	var (
-		queryParams    = make(map[string]string)
-		newQueryParams []string
-	)
+	var queryParams = make(map[string]string)
 
 	if idx := strings.Index(uri, "?"); idx != -1 {
 		params := strings.Split(uri[idx+1:], "&")
@@ -59,6 +128,15 @@ func setUri(c echo.Context, links string) string {
 		}
 	}
 
+	return queryParams
+}
+
+func pageLinks(c echo.Context, links string) string {
+	host := env.Get("APP_URL", fmt.Sprintf("%s://%s", c.Scheme(), c.Request().Host))
+	path := c.Request().URL.Path
+
+	queryParams := setUri(c)
+
 	page := 1
 	if val, ok := queryParams["page"]; ok {
 		if parsed, err := strconv.Atoi(val); err == nil {
@@ -72,8 +150,8 @@ func setUri(c echo.Context, links string) string {
 		page = 2
 	}
 
+	var newQueryParams []string
 	queryParams["page"] = strconv.Itoa(page)
-
 	for k, v := range queryParams {
 		newQueryParams = append(newQueryParams, fmt.Sprintf("%s=%v", k, v))
 	}
@@ -82,7 +160,7 @@ func setUri(c echo.Context, links string) string {
 
 func prevPage(page int, c echo.Context) *string {
 	if page > 1 {
-		url := setUri(c, "prev")
+		url := pageLinks(c, "prev")
 		return &url
 	}
 
@@ -91,7 +169,7 @@ func prevPage(page int, c echo.Context) *string {
 
 func nextPage(page, last_page int, c echo.Context) *string {
 	if last_page > page {
-		url := setUri(c, "next")
+		url := pageLinks(c, "next")
 		return &url
 	}
 
@@ -162,6 +240,7 @@ func (qb *queryBuilder) Paginate(c echo.Context) (map[string]interface{}, error)
 		"current_page":  qb.page,
 		"next_page_url": nextPage(qb.page, int(last_page), c),
 		"prev_page_url": prevPage(qb.page, c),
+		"links":         navigationLinks(c, qb.page, int(last_page)),
 	}
 
 	return paginate, nil
