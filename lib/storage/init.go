1e79122f96e29c7f742bf960b6147cd7e3ef4bbe
fachrunwira <fachrunwirap@gmail.com>
2025-07-17 16:59:08 +0700

    adding reading json files

--- lib/storage/init.go
+++ lib/storage/init.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -125,6 +126,97 @@ func (s *storage) Get(path string) *storage {
 	fullPath := filepath.Join(s.path, path)
 	s.path = fullPath
 	return s
+}
+
+func (s storage) Json() (interface{}, error) {
+	if s.err != nil {
+		return nil, s.err
+	}
+
+	if s.path == "" {
+		return nil, fmt.Errorf("must specified using PublicPath() or StoragePath() first")
+	}
+
+	file, err := os.ReadFile(s.path)
+	if os.IsNotExist(err) {
+		return nil, fmt.Errorf("the specified file is not found")
+	} else if err != nil {
+		return nil, err
+	}
+
+	var obj interface{}
+	if err := json.Unmarshal(file, &obj); err != nil {
+		return nil, err
+	}
+	return obj, nil
+}
+
+func (s storage) SaveJsonInChunk(rows int, outputFile string) error {
+	if s.err != nil {
+		return s.err
+	}
+
+	if s.path == "" {
+		return fmt.Errorf("must specified using PublicPath() or StoragePath() first")
+	}
+
+	if outputFile == "" {
+		return fmt.Errorf("outputFile must be specified")
+	}
+
+	info, err := os.Stat(s.path)
+	if os.IsNotExist(err) {
+		return fmt.Errorf("the specified file is not found")
+	} else if err != nil {
+		return err
+	}
+
+	if info.IsDir() {
+		return fmt.Errorf("given path is not a file")
+	}
+
+	file, err := os.Open(s.path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	t, err := decoder.Token()
+	if err != nil || t != json.Delim('[') {
+		return err
+	}
+
+	var chunk []json.RawMessage
+	chunkIndex := 1
+	total := 0
+
+	saveChunk := func(chunk []json.RawMessage, index int) {
+		if len(chunk) == 0 {
+			return
+		}
+
+		outPath := filepath.Join(s.path, fmt.Sprintf("%s_chunk_%d.json", outputFile, index))
+		data, _ := json.MarshalIndent(chunk, "", "  ")
+		_ = os.WriteFile(outPath, data, 0644)
+	}
+
+	for decoder.More() {
+		var obj json.RawMessage
+		if err != decoder.Decode(&obj) {
+			return err
+		}
+		chunk = append(chunk, obj)
+		total++
+
+		if len(chunk) == rows {
+			saveChunk(chunk, chunkIndex)
+			chunk = nil
+			chunkIndex++
+		}
+	}
+
+	return nil
 }
 
 func (s storage) Csv() ([]string, error) {
